4928501d92cf6beab626e03a45bee2861c364e12
Andrzej <kierski.andrzej@gmail.com>
2017-02-19 11:44:28 +0100

    add functions of todo lists

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,18 +1,42 @@
 'use strict';
 
 $(document).ready(function () {
-  $('.delete-todo').on('click', function (e) {
-    $target = $(e.target);
-    var id = $target.attr('data-id');
-    $.ajax({
-      type: 'GET',
-      url: '/todo/delete/' + id,
-      success: function success(response) {
-        window.location.href = '/';
-      },
-      error: function error(_error) {
-        console.log(_error.responseText);
-      }
-    });
-  });
+
+	// post delete request
+	$('#delete-todo').on('click', function (e) {
+		var id = $(e.target).attr('data-id');
+		$.ajax({
+			type: 'GET',
+			url: '/todo/delete/' + id,
+			success: function success(response) {
+				window.location.href = '/';
+			},
+			error: function error(_error) {
+				console.log(_error.responseText);
+			}
+		});
+	});
+
+	// scroll header
+	var scrollHeader = function scrollHeader() {
+		var header = document.querySelector('.header__bg');
+		window.addEventListener('scroll', function () {
+			var topOffset = document.querySelector('body').scrollTop;
+			header.setAttribute('style', 'left:' + -(topOffset / 10) + '%;');
+		});
+	};
+
+	// typed
+	var typeIntro = function typeIntro() {
+		$(".typed").typed({
+			strings: ["enter tasks", "edit tasks", "remove tasks"],
+			typeSpeed: 100,
+			backDelay: 900,
+			loop: true
+		});
+	};
+
+	// init functions
+	scrollHeader();
+	typeIntro();
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,41 @@
 $(document).ready(() => {
-  $('.delete-todo').on('click', (e) => {
-    $target = $(e.target);
-    const id = $target.attr('data-id');
-    $.ajax({
-      type: 'GET',
-      url: '/todo/delete/' + id,
-      success: (response) => {
-        window.location.href = '/';
-      },
-      error: (error) => {
-        console.log(error.responseText);
-      }
-    });
-  });
+
+	// post delete request
+	$('#delete-todo').on('click', (e) => {
+		const id = $(e.target).attr('data-id');
+		$.ajax({
+			type: 'GET',
+			url: '/todo/delete/' + id,
+			success: (response) => {
+				window.location.href = '/';
+			},
+			error: (error) => {
+				console.log(error.responseText);
+			}
+		});
+	});
+
+	// scroll header
+	const scrollHeader = () => {
+		const header = document.querySelector('.header__bg');
+		window.addEventListener('scroll', function () {
+			const topOffset = document.querySelector('body').scrollTop;
+			header.setAttribute('style', 'left:' + -(topOffset / 10) + '%;');
+		});
+	};
+
+	// typed
+	const typeIntro = () => {
+		$(".typed").typed({
+			strings: ["enter tasks", "edit tasks", "remove tasks"],
+			typeSpeed: 100,
+			backDelay: 900,
+			loop: true
+		});
+	};
+
+  // init functions
+  scrollHeader();
+	typeIntro();
+
 });
